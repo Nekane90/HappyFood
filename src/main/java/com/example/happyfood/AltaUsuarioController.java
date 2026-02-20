92097a2a04d0a80bb335da48aa329f3b380a58dd
maialenperez06 <maialenp14@gmail.com>
2026-02-20 13:17:20 +0100

    Añadido CheckComboBox y diseño de alta usuario

--- src/main/java/com/example/happyfood/AltaUsuarioController.java
+++ src/main/java/com/example/happyfood/AltaUsuarioController.java
@@ -2,13 +2,16 @@ package com.example.happyfood;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.controlsfx.control.CheckComboBox;
+
 
 public class AltaUsuarioController {
     @FXML
     private Label welcomeText;
-
+    private CheckComboBox<String> comboIntolerancias;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        // Así le metes los datos por código
+        comboIntolerancias.getItems().addAll("Gluten", "Lactosa", "Frutos Secos", "Huevo");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.happyfood {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.controlsfx.controls;
 
 
     opens com.example.happyfood to javafx.fxml;
