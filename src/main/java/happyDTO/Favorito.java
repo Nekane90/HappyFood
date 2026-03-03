c940ea42c38bc2ecf6b263317cdf95525ed77f01
nekane-m <nekane.m@gistek-s.com>
2026-03-03 09:25:06 +0100

    añadida clase favoritoDto

---
+++ src/main/java/happyDTO/Favorito.java
@@ -0,0 +1,38 @@
+package happyDTO;
+
+public class Favorito {
+
+    private int id;
+    private int idUsuario;
+    private int idReceta;
+
+    public Favorito(int id, int idUsuario, int idReceta){
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.idReceta = idReceta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdReceta() {
+        return idReceta;
+    }
+
+    public void setIdReceta(int idReceta) {
+        this.idReceta = idReceta;
+    }
+}
