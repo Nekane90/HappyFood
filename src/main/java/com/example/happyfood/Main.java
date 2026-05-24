75c341530d01597f756bc9b66c8716a95e95ae57
maialenperez06 <maialenp14@gmail.com>
2026-05-24 16:24:25 +0200

    ejecutable JAR con Java 23 y gitignore configurado

---
+++ src/main/java/com/example/happyfood/Main.java
@@ -0,0 +1,9 @@
+package com.example.happyfood;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Llamamos directamente al método main de tu clase Launcher
+        Launcher.main(args);
+    }
+}
